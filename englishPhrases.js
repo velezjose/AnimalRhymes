45f71a13da44bbdac61162d18eb870da019a0c73
Jose Velez <josevelezq7@gmail.com>
2019-04-19 22:12:55 -0700

    Add an animal dictionary and English common phrases data sets

---
+++ englishPhrases.js
@@ -0,0 +1,94 @@
+let englishPhrases = `What’s up?
+What’s new?
+Let's go!
+That's dope!
+Wanna go for a drink?
+What have you been up to lately?
+How’s it going?
+How are things?
+How’s life?
+I’m fine, thanks. How about you?
+Pretty good.
+Same as always.
+Not so great.
+Could be better.
+Can't complain.
+I really appreciate it.
+I’m really grateful.
+That’s so kind of you.
+I owe you one.
+No problem.
+Don't worry.
+Don’t mention it.
+My pleasure.
+Anytime.
+It was nice chatting with you.
+Anyway, I should get going.
+Do you have any idea?
+Would you happen to know?
+I don’t suppose you would know?
+I have no idea.
+I have no clue.
+I can’t help you there.
+Beats me.
+I’m not really sure.
+I’ve been wondering that, too.
+I’ve never given it much thought.
+I don’t have strong feelings either way.
+It doesn’t make any difference to me.
+I have no opinion on the matter.
+Exactly.
+Absolutely.
+That’s so true.
+That’s for sure.
+I agree one hundred percent.
+I couldn’t agree with you more.
+Tell me about it!
+You’re telling me!
+I’ll say!
+I suppose so.
+Tight.
+I’m not so sure about that.
+That’s not how I see it.
+Not necessarily
+That’s great!
+How wonderful!
+Awesome!
+Oh no!
+That’s terrible.
+Poor you.
+I’m so sorry to hear that.
+Are you free Saturday night?
+Are you doing anything Friday night?
+Do you wanna see a movie?
+Would you like to join me for dinner?
+I’m starving!
+I’m so hungry.
+Let’s grab a bite.
+How about eating out tonight?
+Peace.
+What's goodly?
+I’ll have it
+It cost a fortune.
+It cost an arm and a leg.
+True.
+That’s a rip-off.
+That’s a bit pricey.
+That’s quite reasonable.
+Word.
+That’s a good deal.
+It was a real bargain.
+It was dirt cheap.
+It’s a little chilly.
+Awesome.
+It’s freezing.
+Make sure to bundle up.
+It’s absolutely boiling!
+it scorching hot outside
+I’m exhausted.
+I’m dead tired.
+I’m beat.
+I can hardly keep my eyes open.
+I’m gonna hit the sack.`
+
+module.exports = englishPhrases.split('\n');
