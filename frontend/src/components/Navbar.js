8dfd020c9ffa20e067f4f64884e98348f8c7afcb
Austin Kemper <austin.kemper@live.com>
2019-06-23 13:01:47 -0700

    New favicon, Navbar and Search components.

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import horsePic from '../assets/horse.png';
+
+class Navbar extends React.Component {
+
+  render() {
+
+    let horsePicStyle = {
+      width: '50px',
+    };
+    let flipImageStyle = {
+      transform: 'scaleX(-1)',
+    };
+    let reverseHorsePicStyle = Object.assign({}, horsePicStyle, flipImageStyle);
+    return (
+      <div class='navbar'>
+        <img src={horsePic} alt='horse' style={horsePicStyle}></img>
+        <h2>Animal Rhymes</h2>
+        <img src={horsePic} alt='horse' style={reverseHorsePicStyle} ></img>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Search extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <h2>Search</h2>
+        <input type='text' placeholder='Of course'></input>
+      </div>
+    );
+  }
+}
+
+export default Search;
