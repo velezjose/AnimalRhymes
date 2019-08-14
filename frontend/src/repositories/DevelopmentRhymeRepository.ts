c0cda677f2bb107c9941e06e22e2580719cfca4b
Austin Kemper <austin.kemper@live.com>
2019-08-13 20:49:20 -0700

    Expand prettier scope, verify server response or throw in DevelopmentRhymeRepository.

--- frontend/src/repositories/DevelopmentRhymeRepository.ts
+++ frontend/src/repositories/DevelopmentRhymeRepository.ts
@@ -1,16 +1,21 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from "axios";
 
 const LOCAL_SAVAGE_RHYMES_URL = "http://localhost:8080/savageRhymes";
 
 export class DevelopmentRhymeRepository {
-  getRhymes = async (query: string) =>{ 
-    const axiosResponse: AxiosResponse = await axios.get(LOCAL_SAVAGE_RHYMES_URL + `?q=${query}`);
-
-    // TODO(ak): validate response or throw
-
-    console.log(axiosResponse);
-    console.log(axiosResponse.data);
-    console.log(axiosResponse.data.results);
+  getRhymes = async (query: string) => {
+    const axiosResponse: AxiosResponse = await axios.get(
+      LOCAL_SAVAGE_RHYMES_URL + `?q=${query}`
+    );
+    if (
+      !axiosResponse ||
+      !axiosResponse.data ||
+      !Array.isArray(axiosResponse.data.results)
+    ) {
+      throw new Error(
+        `SavageRhymes response didn't include a results array: ${axiosResponse}.`
+      );
+    }
     return axiosResponse.data.results;
-  }
+  };
 }
